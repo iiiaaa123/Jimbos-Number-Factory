51f496cf84f3002ab87e9c41886f2cc123a6eb1f
Miser <146992858+iiiaaa123@users.noreply.github.com>
2026-01-21 14:52:57 -0500

    what if the documentation was good actually

--- challenges/Pols-Bane.lua
+++ challenges/Pols-Bane.lua
@@ -8,6 +8,11 @@ SMODS.Challenge {
             { id = polsbane },
         },
     },
+    apply = function(self)
+		G.E_MANAGER:add_event(Event({func = function()
+			{ SMODS.change_voucher_limit(0) }
+		return true end}))
+	end
     restrictions = {
         banned_tags = {
             {id = tag_voucher},
