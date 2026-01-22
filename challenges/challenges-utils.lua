d1ab3035fac4c3aee723c9749e5fb3ab2ffdb051
Miser <146992858+iiiaaa123@users.noreply.github.com>
2026-01-22 14:24:42 -0500

    nvm the nubby func doesn't work

--- challenges/challenges-utils.lua
+++
@@ -1,8 +0,0 @@
-function NUBBY.change_voucher_limit(mod)
-    G.GAME.modifiers.extra_vouchers = (G.GAME.modifiers.extra_vouchers or 0) + mod
-    if G.shop then
-        for i=1, mod do
-            SMODS.add_voucher_to_shop()
-        end
-    end
-end
